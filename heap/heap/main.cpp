c6b1809fc42ba1aee36ba406f1ca3d99e79c43e6
Muhiddin <104346376+Radjabmuhammedov@users.noreply.github.com>
2022-04-25 13:12:44 +0500

    just

---
+++ heap/heap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  heap
+//
+//  Created by MUKHIDDIN RADJABMUKHAMEDOV on 25/04/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+    
+    cout<<"hehihd"<<endl;
+    return 0;
+}
